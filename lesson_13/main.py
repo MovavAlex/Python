a4f0d25d98f1a70ca068d7ad217f15ecdd7ddd22
movavi_school <klimnesterov8@mail.com>
2022-12-11 16:32:25 +0700

    123

---
+++ lesson_13/main.py
@@ -0,0 +1,135 @@
+# a = [1, "qwerty", 2.5]
+# b = set(a)
+# c = len(b)
+# print(c)
+
+
+
+# a = {1, 2, 3, 4}
+# b = {7, 2, 3, 8}
+#
+# y = a | b
+# c = a & b
+# i = a - b
+#
+#
+# print(y, c, i)
+
+#
+# lst = [0,7,14,28,0,7]
+# # 7 14 28 7
+#
+#
+#
+# for i in range(len(lst)):
+#     if lst[i]>lst[i-1]:
+#         print(lst[i], end=' ')
+#
+# print()
+# for i in range(len(lst)):
+#     if lst[i] < lst[i-1]:
+#         print(lst[i], end=' ')
+#
+#
+#
+#
+
+#
+# lst1 = [3, 5, 6, 12, 54]
+# lst2 = [54, 6, 251, 12]
+#
+# a = set(lst1)
+# n = set(lst2)
+# c = a & n
+#
+#
+# b = sorted(c,reverse=False)
+#
+# print(b)
+#
+#
+
+
+
+# lst = [1,2,4,2,6,1,2,9]
+# count = []
+# for i in range(len(lst)):
+#     if lst[i] in count:
+#         print("YES")
+#     else:
+#         count.append(lst[i])
+#         print("NO")
+
+
+#Дан список, найти наибольшее и наименьшее число без встроенных функций
+
+
+# lst = [5,1,6,3,8,7,2,9,4]
+#
+# maximum = lst[0]
+# minimum = lst[0]
+#
+# for i in lst:
+#     if i > maximum:
+#         maximum = i
+#
+#     if i < minimum:
+#         minimum = i
+#
+# print("max: ", maximum)
+# print("min: ", minimum)
+
+
+
+
+o ="...|                    |..."
+p ="...|                    |..."
+a ="...|                    |..."
+b ="...|                    |..."
+c ="...|                    |..."
+d ="...|                    |..."
+e ="....|                  |...."
+f ="....|                  |...."
+g ="....|                  |...."
+h ="....|                  |...."
+i ="....|                  |...."
+j =".....|                |....."
+k =".....|                |....."
+l =".....|                |....."
+m =".....|                |....."
+n ="......|______________|......"
+
+
+kahlua = "k"
+balleys = "b"
+cointreau = "c"
+fire = "!"
+
+
+fir = a.replace(" ","!")
+c1 = b.replace(" ", "c")
+c2 = c.replace(" ", "c")
+c3 = d.replace(" ", "c")
+b1 = e.replace(" ", "b")
+b2 = f.replace(" ", "b")
+b3 = g.replace(" ", "b")
+b4 = h.replace(" ", "b")
+b5 = i.replace(" ", "b")
+k1 = j.replace(" ", "k")
+k2 = k.replace(" ", "k")
+k3 = l.replace(" ", "k")
+k4 = m.replace(" ", "k")
+
+
+print()
+
+
+
+
+
+
+
+
+
+
+
