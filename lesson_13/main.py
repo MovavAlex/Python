80d620418b9317de90e475c135cac3bf454af862
movavi_school <klimnesterov8@gmail.com>
2022-12-18 16:27:47 +0700

    18.12.22

--- lesson_13/main.py
+++ lesson_13/main.py
@@ -81,55 +81,185 @@
 
 
 
-
-o ="...|                    |..."
-p ="...|                    |..."
-a ="...|                    |..."
-b ="...|                    |..."
-c ="...|                    |..."
-d ="...|                    |..."
-e ="....|                  |...."
-f ="....|                  |...."
-g ="....|                  |...."
-h ="....|                  |...."
-i ="....|                  |...."
-j =".....|                |....."
-k =".....|                |....."
-l =".....|                |....."
-m =".....|                |....."
-n ="......|______________|......"
-
-
-kahlua = "k"
-balleys = "b"
-cointreau = "c"
-fire = "!"
-
-
-fir = a.replace(" ","!")
-c1 = b.replace(" ", "c")
-c2 = c.replace(" ", "c")
-c3 = d.replace(" ", "c")
-b1 = e.replace(" ", "b")
-b2 = f.replace(" ", "b")
-b3 = g.replace(" ", "b")
-b4 = h.replace(" ", "b")
-b5 = i.replace(" ", "b")
-k1 = j.replace(" ", "k")
-k2 = k.replace(" ", "k")
-k3 = l.replace(" ", "k")
-k4 = m.replace(" ", "k")
-
-
-print()
-
-
+#
+# o ="...|                    |..."
+# p ="...|                    |..."
+# a ="...|                    |..."
+# b ="...|                    |..."
+# c ="...|                    |..."
+# d ="...|                    |..."
+# e ="....|                  |...."
+# f ="....|                  |...."
+# g ="....|                  |...."
+# h ="....|                  |...."
+# i ="....|                  |...."
+# j =".....|                |....."
+# k =".....|                |....."
+# l =".....|                |....."
+# m =".....|                |....."
+# n ="......|______________|......"
+#
+#
+# kahlua = "k"
+# balleys = "b"
+# cointreau = "c"
+# fire = "!"
+#
+#
+# fir = a.replace(" ","!")
+# c1 = b.replace(" ", "c")
+# c2 = c.replace(" ", "c")
+# c3 = d.replace(" ", "c")
+# b1 = e.replace(" ", "b")
+# b2 = f.replace(" ", "b")
+# b3 = g.replace(" ", "b")
+# b4 = h.replace(" ", "b")
+# b5 = i.replace(" ", "b")
+# k1 = j.replace(" ", "k")
+# k2 = k.replace(" ", "k")
+# k3 = l.replace(" ", "k")
+# k4 = m.replace(" ", "k")
+#
+#
+# print()
 
 
 
 
 
+# str(строка)
+# int(целые числа)
+# float(дробные числа)
+# bool(буллевый тип данных)
+# list(список "[]")
+# set(множество "{}")
+# tuple(кортеж "()")
 
 
 
 
+
+# dict1 = {"Ключ1": 1,
+#          "Ключ2": "Антон",
+#          "Якрут": True,
+#          "Словарь": {"Вл1": 1}
+#          } #пример создания словаря
+# empty_dictionary1 = {} # 1 способ создания пустого словарф
+# empty_dictionary2 = dict()  # 2 способ создания пустого словарф
+#
+#
+# print(dict1["Словарь"]["Вл1"])
+#
+#
+
+
+# Циклы
+
+
+# while и for
+# x = 0
+# b = 0
+# while x != 5:
+#     b += 1
+#     print(b)
+#     x += 1
+# while True:
+#     print(123)
+# while False:
+#     print(123)
+#
+
+# phrase = "фраза"
+#
+# # for supra in phrase:
+# #     print(supra)
+
+#
+# for jojo in range(5):
+#     print(jojo)
+#
+#
+
+
+# empty_dict = dict() #единственный способ создания пустого множества
+# set1 = {1,2,3}
+# print(type(set1))
+
+# set0 = {1,1,1,2}
+# print(set0)
+# set00 = {"A","B","Ц"}
+# print(set00)
+#
+#
+# set1 = {1,2,3,4,5}
+# set2 = {3,4,5,6,7}
+#
+#
+#
+#
+#
+#
+#
+#
+# print(set1.union(set2))
+# print(set1 | set2)
+#
+# print(set1.intersection(set2))
+# print(set1 & set2)
+#
+# print(set1.difference(set2))
+# print(set1 - set2)
+#
+# #Симметрическая разность
+#
+# print(set1.symmetric_difference(set2))
+
+
+
+
+
+from random import randint
+#
+# lst = []
+# # count = 0
+# # while count!=5:
+# #     lst.append(randint(1,5))
+# #     count += 1
+#
+# for i in range(5):
+#     lst.append(randint(1,5))
+#
+# print(lst)
+#
+# lst1 = set(lst)
+#
+# print(f"Штук: {len(lst1)} {lst1}")
+#
+#
+
+
+
+
+lst1 = []
+lst2 = []
+count = 0
+
+for i in range(randint(100, 1000)):
+    lst1.append(randint(0, 10000))
+    lst2.append(randint(0, 10000))
+    count += 1
+set1 = set(lst1)
+set2 = set(lst2)
+
+a = set1 & set2
+b = max(a)
+c = min(a)
+d = set1 | set2
+d1 = list(d)
+d1.sort()
+
+print(f"Общих чисел: {len(a)}")
+print(f"Кол-во генераций: {count}")
+print(f"Максимальное значение: {b}")
+print(f"Минимальное значение: {c}")
+print(d1)
